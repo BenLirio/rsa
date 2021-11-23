bbcb34c35341ead63d9c20d87054faa92528c5d4
Ben Lirio <benjlirio@gmail.com>
2021-11-23 17:05:00 -0500

    Refactor the unit64 int bigInt

--- main.c
+++ main.c
@@ -6,6 +6,10 @@
 
 typedef unsigned long int uint64;
 
+typedef struct bigInt {
+    uint64* data;
+    int len;
+} bigInt;
 
 void panic(char* msg) {
     printf("%s\n", msg);
@@ -29,46 +33,43 @@ bool has_carry(uint64 A, uint64 B) {
     return rtn;
 }
 
-void mul(uint64* dst, uint64* A, uint64* B, int* dst_size, int size_A, int size_B) {
-    uint64 M[size_A][size_B][2];
-    for (int i = 0; i < size_A; i++) {
-        for (int j = 0; j < size_B; j++) {
+
+void mul(bigInt* dst, bigInt A, bigInt B) {
+    dst->len = A.len+B.len;
+    dst->data = calloc(8, dst->len);
+    for (int i = 0; i < dst->len; i++) {
+        dst->data[i] = 0;
+    }
+    uint64 M[A.len][B.len][2];
+    for (int i = 0; i < A.len; i++) {
+        for (int j = 0; j < B.len; j++) {
             for (int k = 0; k < 2; k++) {
                 M[i][j][k] = 0;
             }
         }
     }
-    for (int i = 0; i < size_A; i++) {
-        for (int j = 0; j < size_B; j++) {
-            M[i][j][0] = mul_lo(A[i], B[j]);
-            M[i][j][1] = mul_hi(A[i], B[j]);
+    for (int i = 0; i < A.len; i++) {
+        for (int j = 0; j < B.len; j++) {
+            M[i][j][0] = mul_lo(A.data[i], B.data[j]);
+            M[i][j][1] = mul_hi(A.data[i], B.data[j]);
         }
     }
-    for (int i = 0; i < size_A+size_B; i++) {
-        dst[i] = 0;
-    }
-    uint64* carry = calloc(8, size_A+size_B);
-    for (int i = 0; i < size_A; i++) {
-        for (int j = 0; j < size_B; j++) {
+    uint64* carry = calloc(8, dst->len);
+    for (int i = 0; i < A.len; i++) {
+        for (int j = 0; j < B.len; j++) {
             for (int k = 0; k < 2; k++) {
-                if (has_carry(dst[i+j+k], M[i][j][k])) {
-                    carry[i+j+k+1] += 1;
-                }
-                dst[i+j+k] += M[i][j][k];
+                carry[i+j+k+1] += has_carry(dst->data[i+j+k], M[i][j][k]);
+                dst->data[i+j+k] += M[i][j][k];
             }
         }
     }
-    for (int i = 0; i < size_A+size_B; i++) {
-        if (has_carry(dst[i], carry[i])) {
-            carry[i+1] += 1;
-        }
-        dst[i] += carry[i];
+    for (int i = 0; i < dst->len; i++) {
+        carry[i+1] += has_carry(dst->data[i], carry[i]);
+        dst->data[i] += carry[i];
     }
-
-    *dst_size = size_A+size_B;
-    if (dst[size_A+size_B-1] == 0) {
-        dst = realloc(dst, (size_A+size_B-1)<<3);
-        *dst_size = size_A+size_B-1;
+    if (dst->data[dst->len-1] == 0) {
+        dst->len -= 1; 
+        dst->data = realloc(dst->data, (dst->len)<<3);
     }
 }
 
@@ -79,14 +80,13 @@ void show(uint64* num, int n) {
     printf("\n");
 }
 
-void test(uint64* nums[], int* nums_size) {
-    uint64* D = calloc(8, nums_size[0]+nums_size[1]);
-    int D_size = 0;
-    mul(D, nums[0], nums[1], &D_size, nums_size[0], nums_size[1]);
+void test(bigInt nums[]) {
+    bigInt* D = malloc(sizeof(bigInt));
+    mul(D, nums[0], nums[1]);
     for (int i = 0; i < 3; i++) {
-        show(nums[i], nums_size[i]);
+        show(nums[i].data, nums[i].len);
     }
-    show(D, D_size);
+    show(D->data, D->len);
     printf("\n");
     
     return;
@@ -96,20 +96,19 @@ int main() {
     int fp = open("testdata/small/numbers", O_RDONLY);
     int n;
     for (;;) {
-        uint64* nums[3];
-        int nums_size[3];
+        bigInt nums[3];
         for (int i = 0; i < 3; i++) {
             if (read(fp, &n, sizeof(n)) == sizeof(n)) {
-                nums[i] = calloc(8, (n+7)/8);
-                if ((read(fp, nums[i], n) != n)) {
+                nums[i].data = calloc(8, (n+7)/8);
+                if ((read(fp, nums[i].data, n) != n)) {
                     panic("Could not read Num");
                 }
-                nums_size[i] = (n+7)/8;
+                nums[i].len = (n+7)/8;
             } else {
                 goto DONE;
             }
         }
-        test(nums, nums_size);
+        test(nums);
     }
 DONE:
     return 0;
