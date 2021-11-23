b62a2d563c96cf7f55050565033b5c6597311887
Ben Lirio <benjlirio@gmail.com>
2021-11-23 15:37:45 -0500

    Working

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef unsigned long int dword;
+typedef unsigned long int uint64;
 
 
 void panic(char* msg) {
@@ -12,27 +12,25 @@ void panic(char* msg) {
     exit(1);
 }
 
-dword low64_mul(dword A, dword B) {
+uint64 mul_lo(uint64 A, uint64 B) {
     return A*B;
 }
-dword hi64_mul(dword A, dword B) {
-    dword mask32 = ((dword)1<<32)-1;
-    dword out = 0;
-    dword p0 = (A&mask32)*(B&mask32);
-    dword p1 = (A>>32)*(B&mask32);
-    dword p2 = (A&mask32)*(B&mask32);
-    dword p3 = (A>>32)*(B>>32);
-    dword cy = ((p0>>32) + (p1&mask32) + (p2&mask32))>>32;
-    return p3+(p1>>32)+(p2>>32) + cy;
+
+uint64 mul_hi(uint64 A, uint64 B) {
+    __asm__(
+            "umulh x0, x0, x1"
+   );
+    register uint64 res asm("x0");
+    return res;
 }
 
-bool has_carry(dword A, dword B) {
+bool has_carry(uint64 A, uint64 B) {
     bool rtn = ((A&B)|((A|B)&(~(A+B))))>>63 == 1;
     return rtn;
 }
 
-void mul(dword* dst, dword* A, dword* B, int* dst_size, int size_A, int size_B) {
-    dword M[size_A][size_B][2];
+void mul(uint64* dst, uint64* A, uint64* B, int* dst_size, int size_A, int size_B) {
+    uint64 M[size_A][size_B][2];
     for (int i = 0; i < size_A; i++) {
         for (int j = 0; j < size_B; j++) {
             for (int k = 0; k < 2; k++) {
@@ -42,14 +40,14 @@ void mul(dword* dst, dword* A, dword* B, int* dst_size, int size_A, int size_B) 
     }
     for (int i = 0; i < size_A; i++) {
         for (int j = 0; j < size_B; j++) {
-            M[i][j][0] = low64_mul(A[i], B[j]);
-            M[i][j][1] = hi64_mul(A[i], B[j]);
+            M[i][j][0] = mul_lo(A[i], B[j]);
+            M[i][j][1] = mul_hi(A[i], B[j]);
         }
     }
     for (int i = 0; i < size_A+size_B; i++) {
         dst[i] = 0;
     }
-    dword* carry = calloc(8, size_A+size_B);
+    uint64* carry = calloc(8, size_A+size_B);
     for (int i = 0; i < size_A; i++) {
         for (int j = 0; j < size_B; j++) {
             for (int k = 0; k < 2; k++) {
@@ -60,14 +58,12 @@ void mul(dword* dst, dword* A, dword* B, int* dst_size, int size_A, int size_B) 
             }
         }
     }
-    /*
     for (int i = 0; i < size_A+size_B; i++) {
         if (has_carry(dst[i], carry[i])) {
             carry[i+1] += 1;
         }
         dst[i] += carry[i];
     }
-    */
 
     *dst_size = size_A+size_B;
     if (dst[size_A+size_B-1] == 0) {
@@ -76,15 +72,15 @@ void mul(dword* dst, dword* A, dword* B, int* dst_size, int size_A, int size_B) 
     }
 }
 
-void show(dword* num, int n) {
+void show(uint64* num, int n) {
     for (int i = n-1; i >= 0; i--) {
         printf("%016lx ", num[i]);
     }
     printf("\n");
 }
 
-void test(dword* nums[], int* nums_size) {
-    dword* D = calloc(8, nums_size[0]+nums_size[1]);
+void test(uint64* nums[], int* nums_size) {
+    uint64* D = calloc(8, nums_size[0]+nums_size[1]);
     int D_size = 0;
     mul(D, nums[0], nums[1], &D_size, nums_size[0], nums_size[1]);
     for (int i = 0; i < 3; i++) {
@@ -100,7 +96,7 @@ int main() {
     int fp = open("testdata/small/numbers", O_RDONLY);
     int n;
     for (;;) {
-        dword* nums[3];
+        uint64* nums[3];
         int nums_size[3];
         for (int i = 0; i < 3; i++) {
             if (read(fp, &n, sizeof(n)) == sizeof(n)) {
